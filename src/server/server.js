a5c0b2173b9b1043e6ad6403975ebc5e7777f551
Alexander Cai <alexandercai@outlook.com>
2019-06-10 08:43:06 -0600

    got server running, no logic yet

--- src/server/server.js
+++ src/server/server.js
@@ -1,21 +1,96 @@
-const path = require('path');
-const jsdom = require('jsdom');
-const express = require('express');
-const app = express();
-const server = require('http').Server(app);
-const io = require('socket.io').listen(server);
+// reads in our .env file and makes those values available as environment variables
+require('dotenv').config();
+
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
 const Datauri = require('datauri');
+const express = require('express');
+const jsdom = require('jsdom');
+const mongoose = require('mongoose');
+const passport = require('passport');
+const path = require('path');
+
+// We set up our express server
+const app = express();
+const server = require('http').createServer(app);
+const io = require('socket.io').listen(server);
+
+// Load routes from other files
+const routes = require('./routes/main');
+const secureRoutes = require('./routes/secure');
+const passwordRoutes = require('./routes/password');
+
+const asyncMiddleware = require('./middleware/asyncMiddleware');
+const ChatModel = require('./models/chatModel');
 
 const datauri = new Datauri();
 const { JSDOM } = jsdom;
 
-app.use(express.static(__dirname + '/public'));
+// setup mongo connection
+const uri = process.env.MONGODB_URI;
+mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true });
+mongoose.connection
+  .on('error', (error) => {
+    console.error(error);
+    process.exit(1);
+  })
+  .on('connected', () => {
+    console.log('connected to mongo');
+  });
+mongoose.set('useFindAndModify', false);
 
-app.get('/', function (req, res) {
-  res.sendFile(__dirname + '/index.html');
+// Load in some middlewares to make sure we can read incoming requests properly
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(cookieParser());
+
+require('./auth/auth');
+
+// Serve our static files and set up our main routes (see ./routes)
+app.use(express.static(path.resolve(__dirname, '../../build')));
+app.use('/', routes);
+app.use('/', passwordRoutes);
+app.use('/', passport.authenticate('jwt', { session: false }), secureRoutes);
+// catch all other routes
+app.use((req, res, next) => {
+  res.status(404).json({ message: '404 - Not Found' });
+});
+// handle errors
+app.use((err, req, res, next) => {
+  console.error(err.message);
+  res.status(err.status || 500).json({ error: err.message });
 });
 
+// Specific paths when the user tries to access certain parts
+
+app.get('/', function (req, res) {
+  res.sendFile(path.resolve(__dirname, '../../build/index.html'));
+});
+app.get('/game.html', passport.authenticate('jwt', { session: false }), (req, res) => {
+  res.sendFile(path.resolve(__dirname, '../../build/game.html'))
+});
+app.get('/messages', passport.authenticate('jwt', { session: false }), asyncMiddleware(async (req, res, next) => {
+  const messages = await ChatModel.find({}, 'email name message createdAt -_id')
+    .sort({ createdAt: -1 })
+    .limit(30);
+  res.status(200).json(messages.reverse());
+}));
+
+// We put this here to have access to SocketIO
+app.post('/submit-chatline', passport.authenticate('jwt', { session: false }), asyncMiddleware(async (req, res, next) => {
+  const { message } = req.body;
+  const { email, name } = req.user;
+  await ChatModel.create({ email, name, message });
+  io.emit('newMessage', {
+    username: name,
+    message,
+  });
+  res.status(200).json({ status: 'ok' });
+}));
+
 function setupAuthoritativePhaser() {
+  console.log('setting up authoritative server');
+
   JSDOM.fromFile(path.join(__dirname, 'authoritative-server/index.html'), {
     // To run the scripts in the html file
     runScripts: "dangerously",
@@ -24,6 +99,7 @@ function setupAuthoritativePhaser() {
     // So requestAnimatinFrame events fire
     pretendToBeVisual: true
   }).then((dom) => {
+    console.log('finished loading file');
     dom.window.URL.createObjectURL = (blob) => {
       if (blob){
         return datauri.format(blob.type, blob[Object.getOwnPropertySymbols(blob)[0]]._buffer).content;
@@ -31,8 +107,8 @@ function setupAuthoritativePhaser() {
     };
     dom.window.URL.revokeObjectURL = (objectURL) => {};
     dom.window.gameLoaded = () => {
-      server.listen(8082, function () {
-        console.log(`Listening on ${server.address().port}`);
+      server.listen(8080, () => {
+        console.log(`Listening on http://localhost:${server.address().port}`);
       });
     };
     dom.window.io = io;
